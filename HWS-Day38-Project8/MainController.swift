12bced423b60df667421b4883ee4ac30bb7e9079
Steven Jemmott <jemmott.steven@gmail.com>
2019-11-13 00:09:10 -0400

    Initial commit - setup project to use iPad layout

---
+++ HWS-Day38-Project8/MainController.swift
@@ -0,0 +1,59 @@
+//
+//  MainController.swift
+//  HWS-Day38-Project8
+//
+//  Created by Steven Jemmott on 12/11/2019.
+//  Copyright © 2019 Lagom Exp. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+    
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemYellow
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> UIViewController {
+//            let navController = UINavigationController(rootViewController: MainController())
+            return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+            
+        }
+    }
+}
