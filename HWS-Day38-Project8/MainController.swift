acf9550282cb8728efa0e12bce77830f4718cf30
Steven Jemmott <jemmott.steven@gmail.com>
2019-11-13 17:35:23 -0400

    Refactored scoreLabel to update using property observers

--- HWS-Day38-Project8/MainController.swift
+++ HWS-Day38-Project8/MainController.swift
@@ -19,7 +19,11 @@ class MainController: UIViewController {
     var activatedButtons = [UIButton]()
     var solutions = [String]()
     
-    var score = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
     
     override func loadView() {
@@ -151,7 +155,6 @@ class MainController: UIViewController {
             
             currentAnswer.text = ""
             score += 1
-            scoreLabel.text = "Score: \(score)"
             
             if score % 7 == 0 {
                 let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
@@ -164,7 +167,7 @@ class MainController: UIViewController {
     func levelUp(action: UIAlertAction) {
         level += 1
         solutions.removeAll(keepingCapacity: true)
-        scoreLabel.text = "Score: 0"
+        score = 0
         loadLevel()
         
         for button in letterButtons {
